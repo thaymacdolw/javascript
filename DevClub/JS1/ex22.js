14eeee3f1d33da5c39f40b8456eb1a1cfaebddbb
thaymacdolw <thaymacdolw@gmail.com>
2023-02-23 14:48:45 -0300

    Create ex22.js

---
+++ DevClub/JS1/ex22.js
@@ -0,0 +1,1 @@
+// Faça um programa onde ele leia 4 números. Em cada caso, ele deve retornar a mensagem condizente: - Todos os números são impares => TODOS OS NÚMEROS SÃO IMPARES - Todos os números são pares => TODOS OS NÚMEROS SÃO PARES. Em caso negativo dos casos a cima, colocar uma mensagem => Há números pares e ímpares.
