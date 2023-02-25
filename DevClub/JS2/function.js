bb3f5ae5406eba253a0543e854157b45d3fb4493
thaymacdolw <thaymacdolw@gmail.com>
2023-02-25 17:54:20 -0300

    learning 'bout functions

---
+++ DevClub/JS2/function.js
@@ -0,0 +1,18 @@
+// Funções 
+
+function printingAboutDevClub() {
+    console.log('Estou no DevClub')
+}
+printingAboutDevClub()
+
+// Funções : Parametros e Argumentos
+
+
+function sum(number1, number2) { //parametros
+    console.log(number1 + number2)
+}
+sum(2, 3) // arguemntos, enviam para os parametros o valor, assim podemos imprimir no console.log
+
+
+
+
---
+++ DevClub/JS2/functionsreturn.js
@@ -0,0 +1,15 @@
+// Funções : Return
+
+function sum(number1, number2){ //parametros
+    return number1 + number2
+}
+
+const firstNumber = 10
+const secondNumber = 20
+
+console.log(`O primeiro número é ${firstNumber}`)
+console.log(`O segundo número é ${secondNumber}`)
+
+console.log(`A soma é ${sum(firstNumber,secondNumber)}`)
+
+//funcao sem return sao chamadas de VOID
