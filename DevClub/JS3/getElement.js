928fddcc8aed3dce3ff38be1f5b5d363f6eff74b
thaymacdolw <thaymacdolw@gmail.com>
2023-05-25 19:44:00 +0200

    beggining getElement

---
+++ DevClub/JS3/getElement.js
@@ -0,0 +1,24 @@
+
+/* No JS o HTML é chamado de "document", entao quando for chamar aqui noJS, sempre document.getElement...
+
+document -> HTML
+
+getElementByID -> trás UM elemento pelo ID
+getElementByClassName -> trás TODOS os elementos com essa classe
+getElementByTagName -> trás TODOS os elementos com essa TAG
+getElementByName -> trás TODOS os elementos com esse NAME
+
+querySelector -> trás UM elemento, o PRIMEIRO que encontrar
+querySelectorALL -> trás TODOS os elementos que encontrar
+
+*/
+
+
+/* EXEMPLOS
+
+getElementById :
+
+const input = document.getElementById("nomedaID") 
+console.log (input)
+
+*/
