6a7f3760ed29e8019199bd388f4d9973c533ae18
Thayna Mac Dolw <thaymacdolw@gmail.com>
2023-10-24 20:10:16 +0200

    map part 2

--- DevClub/JS4/Map.js
+++ DevClub/JS4/Map.js
@@ -1,8 +1,4 @@
-
-
-
-
-const list = [
+/*const list = [
     {name: 'Rodolfo', vip: true},
     {name: 'Maria', vip: false},
     {name: 'Joao', vip: true},
@@ -13,19 +9,36 @@ const list = [
 ]
 
 const newList = list.map( user => {
-   let braceletColor 
-
-   if(braceletColor === true) {
-    braceletColor = 'black'
-   } else {
-    braceletColor = 'green'
-   }
+  
    const newUser = {
     name: user.name,
-    braceletColor: braceletColor
+    braceletColor: user.vip ? 'black' : 'green'
    }
 
     return newUser
 })
 
 console.log(newList)
+*/
+
+const students = [
+    { name: 'Rodolfo', testGrade: 7},
+    { name: 'Maria', testGrade: 5},
+    { name: 'João', testGrade: 8},
+    { name: 'Bruno', testGrade: 9},
+    { name: 'Carla', testGrade: 3},
+    { name: 'Ana', testGrade: 2},
+    { name: 'Julio', testGrade: 10},
+
+]
+
+const newStudents = students.map ( student => {
+    return {
+        name: student.name,
+        studentStatus: student.testGrade >= 6 ? 'Aprovado' : 'Reprovado'
+    }
+    
+})
+
+console.log(newStudents)
+
