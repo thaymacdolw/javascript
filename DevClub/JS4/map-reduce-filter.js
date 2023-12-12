b1cc5f14b4b3ed348f7f17c42c9b5585d9d685cb
Thayna Mac Dolw <thaymacdolw@gmail.com>
2023-12-12 19:04:35 +0100

    Create map-reduce-filter.js

---
+++ DevClub/JS4/map-reduce-filter.js
@@ -0,0 +1,46 @@
+const companies = [
+    { name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975, },
+    { name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968 },
+    { name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004 },
+    { name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006 },
+    { name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976 },
+]
+
+// Adicionar 10% de valor de mercado a todas as companhias = Map
+// Filtrar somente companhias fundadas antes de 1990 = Filter
+// Somar o valor de mercado dos restantes = Reduce
+
+/*const marketValueOldCompanies = companies.map(company => {
+    company.marketValue = company.marketValue + (company.marketValue * 0.1)
+
+    return company
+}).filter((company) =>company.foundedOn < 1990).reduce((acc, company) => acc + company.marketValue,0)
+
+console.log(marketValueOldCompanies)
+*/
+//OUTRO MODO DE FAZER
+
+const add10Percent = (company) => {
+    company.marketValue = company.marketValue + (company.marketValue * 0.1)
+    return company
+}
+
+
+const filterCompanies = (company) => company.foundedOn < 1990
+
+const calculateTotalMarketValue = (acc, company) => acc + company.marketValue
+
+const marketValueOldCompanies = companies
+.map(add10Percent)
+.filter(filterCompanies)
+.reduce(calculateTotalMarketValue,0)
+
+console.log(marketValueOldCompanies)
+
+
+
+
+//Subtrair 10% de valor de mercado a todas as empresas
+//Filtrar somente companhias fundadas após 1980
+// Somar o valor de mercado das restantes
