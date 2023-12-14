2a45e4dee97bb59a2fb62bdff136f96277ff70c7
Thayna Mac Dolw <thaymacdolw@gmail.com>
2023-12-14 21:11:49 +0100

    html+css do projeto

---
+++ DevClub/JS4/Desafio-do-Módulo/products.js
@@ -0,0 +1,11 @@
+
+
+const menuOptions = [
+    { name: 'X-Salad', price: 10, vegan: false, src: './img/xsalada.jpeg' },
+    { name: 'X-Bacon', price: 14, vegan: false, src: './img/xbacon.png' },
+    { name: 'X-Banco Egg', price: 19, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'Monster', price: 30, vegan: false, src: './img/monstruoso.png' },
+    { name: 'Big Vegan', price: 35, vegan: true, src: './img/xvegan.png' },
+    { name: 'X-Vegan', price: 25, vegan: ftrue, src: './img/monstruoso-vegan.png' },
+
+]
